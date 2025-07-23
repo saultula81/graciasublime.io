a4c97ce50a063688a1af74a8136997829de27151
saultula81 <93384660+saultula81@users.noreply.github.com>
2025-07-23 09:21:49 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function (e) {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', function (event) {
-  // Deja pasar las solicitudes normales
-});
